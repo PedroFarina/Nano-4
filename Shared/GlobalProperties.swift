fdc43e8af87aaceb2360a1f21fe322f7272507dd
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-04-15 15:56:01 -0300

    Adding multipeer

---
+++ Shared/GlobalProperties.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalStrings.swift
+//  Nano4
+//
+//  Created by Pedro Giuliano Farina on 15/04/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+public struct GlobalProperties {
+    private init(){}
+    public static let serviceType: String = "PGFNano4"
+}
